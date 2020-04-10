d3800b1bd5bca05256404de602f9c744a42b37e8
geoccifer <51718547+geoccifer@users.noreply.github.com>
2020-04-09 20:24:17 -0500

    Added Function
    Function

--- rpsGame.py
+++ rpsGame.py
@@ -4,65 +4,68 @@ import sys
 print("ROCK, PAPER, SCISSORS")
 
 # Keep track of the number of wins, losses, and ties
-wins = 0
-losses = 0
-ties = 0
 
-while True:  # The main game loop
-    print("%s Wins, %s Losses, %s Ties" % (wins, losses, ties))
-    while True:
-        print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
-        playerMove = input()
-        if playerMove == "q":
-            sys.exit()  # Quit the program
-        if playerMove == "r" or playerMove == "p" or playerMove == "s":
-            break  # resumes execution at the next statement
-    print("Type one of r, p, s, or q.")
+
+def main():
+    wins = 0
+    losses = 0
+    ties = 0
+    while True:  # The main game loop
+        print("%s Wins, %s Losses, %s Ties" % (wins, losses, ties))
+        while True:
+            print("Enter your move: (r)ock (p)aper (s)cissors or (q)uit")
+            playerMove = input()
+            if playerMove == "q":
+                sys.exit()  # Quit the program
+            if playerMove == "r" or playerMove == "p" or playerMove == "s":
+                break  # resumes execution at the next statement
+        print("Type one of r, p, s, or q.")
 
 # Display what the player chose:
 
-
-def playerchose():
-    if playerMove == "r":
-        print("ROCK versus...")
-    elif playerMove == "p":
-        print("PAPER versus...")
-    elif playerMove == "s":
-        print("SCISSORS versus...")
+        if playerMove == "r":
+            print("ROCK versus...")
+        elif playerMove == "p":
+            print("PAPER versus...")
+        elif playerMove == "s":
+            print("SCISSORS versus...")
 
 # Display what the computer chose:
-def compchose():
-    randomNumber = random.randint(1, 3)
-    if randomNumber == 1:
-        computerMove = "r"
-        print("ROCK")
-    elif randomNumber == 2:
-        computerMove = "p"
-        print("PAPER")
-    elif randomNumber == 3:
-        computerMove = "s"
-        print("SCISSORS")
+
+        randomNumber = random.randint(1, 3)
+        if randomNumber == 1:
+            computerMove = "r"
+            print("ROCK")
+        elif randomNumber == 2:
+            computerMove = "p"
+            print("PAPER")
+        elif randomNumber == 3:
+            computerMove = "s"
+            print("SCISSORS")
 
 # Display and record the win/loss/tie:
-def WLT():
-    if playerMove == computerMove:
-        print("It is a tie!")
-        ties = ties + 1
-    elif playerMove == "r" and computerMove == "s":
-        print("You win!")
-        wins = wins + 1
-    elif playerMove == "p" and computerMove == "r":
-        print("You win!")
-        wins = wins + 1
-    elif playerMove == "s" and computerMove == "p":
-        print("You win!")
-        wins = wins + 1
-    elif playerMove == "r" and computerMove == "p":
-        print("You lose!")
-        losses = losses + 1
-    elif playerMove == "p" and computerMove == "s":
-        print("You lose!")
-        losses = losses + 1
-    elif playerMove == "s" and computerMove == "r":
-        print("You lose!")
-        losses = losses + 1
+
+        if playerMove == computerMove:
+            print("It is a tie!")
+            ties = ties + 1
+        elif playerMove == "r" and computerMove == "s":
+            print("You win!")
+            wins = wins + 1
+        elif playerMove == "p" and computerMove == "r":
+            print("You win!")
+            wins = wins + 1
+        elif playerMove == "s" and computerMove == "p":
+            print("You win!")
+            wins = wins + 1
+        elif playerMove == "r" and computerMove == "p":
+            print("You lose!")
+            losses = losses + 1
+        elif playerMove == "p" and computerMove == "s":
+            print("You lose!")
+            losses = losses + 1
+        elif playerMove == "s" and computerMove == "r":
+            print("You lose!")
+            losses = losses + 1
+
+
+main()
